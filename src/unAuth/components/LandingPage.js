a8a36982390e6cd6d082e2f52470b2f7c6548af3
Ahmand Edmonds <ahmandedmonds@32:70:90:69:85:b4.local.ftr>
2026-06-15 20:15:45 -0400

    Functions Added

--- src/unAuth/components/LandingPage.js
+++ src/unAuth/components/LandingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
     <>
       <main className="landing-page">
         <div className="landing-page__card">
-          <h1 className="landing-page__title">rumbxo's AI YouTube Timestamps</h1>
+          <h1 className="landing-page__title">Rumbxo's AI YouTube Timestamps</h1>
           <p className="landing-page__subtitle">
             Welcome to your go-to tool for generating AI-powered timestamps for YouTube videos. Whether you're a content creator looking to enhance your videos or a viewer wanting to navigate through long videos more efficiently, our AI YouTube Timestamps feature has got you covered. Simply paste a YouTube link or video ID, and let our AI do the rest. Get started now and experience the future of video navigation!
           </p>
